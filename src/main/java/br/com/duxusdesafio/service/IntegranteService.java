7c6dc0c7e3e14a47daf0b4053ebbfbb59c146f64
Kell <gilsonvigilante@outlook.com>
2026-09-08 18:32:51 -0300

    feat: cri aservice de integrante com lógica de persistencia adicionada

--- src/main/java/br/com/duxusdesafio/service/IntegranteService.java
+++ src/main/java/br/com/duxusdesafio/service/IntegranteService.java
@@ -1,4 +1,21 @@
 package br.com.duxusdesafio.service;
 
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.repository.IntegranteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class IntegranteService {
+
+    private final IntegranteRepository integranteRepository;
+
+    public IntegranteService(
+            IntegranteRepository integranteRepository) {
+
+        this.integranteRepository = integranteRepository;
+    }
+
+    public Integrante cadastrar(Integrante integrante) {
+        return integranteRepository.save(integrante);
+    }
 }
