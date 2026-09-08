be4b1e5c15625fe21d1a2b882dee84233375232a
Kell <gilsonvigilante@outlook.com>
2026-09-08 18:31:49 -0300

    feat: cria controller de integrante com endpoint de cadastro

--- src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
+++ src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
@@ -1,4 +1,36 @@
 package br.com.duxusdesafio.controller;
 
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.service.IntegranteService;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/integrantes")
 public class IntegranteController {
+
+    private final IntegranteService integranteService;
+
+    public IntegranteController(IntegranteService integranteService) {
+        this.integranteService = integranteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integrante> cadastrar(
+            @RequestBody @Valid Integrante integrante) {
+
+        Integrante integranteCriado =
+                integranteService.cadastrar(integrante);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(integranteCriado);
+    }
 }
---
+++ src/main/java/br/com/duxusdesafio/repository/IntegranteRepository.java
@@ -0,0 +1,4 @@
+package br.com.duxusdesafio.repository;
+
+public interface IntegranteRepository {
+}
