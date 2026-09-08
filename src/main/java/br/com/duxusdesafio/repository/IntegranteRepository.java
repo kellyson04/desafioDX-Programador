df25536397cce874517f58e33ed02682a51b5371
Kell <gilsonvigilante@outlook.com>
2026-09-08 18:32:04 -0300

    feat: cria repositorio de integrante pra persistencia no banco

--- src/main/java/br/com/duxusdesafio/repository/IntegranteRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/IntegranteRepository.java
@@ -1,4 +1,8 @@
 package br.com.duxusdesafio.repository;
 
-public interface IntegranteRepository {
+import br.com.duxusdesafio.model.Integrante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntegranteRepository
+        extends JpaRepository<Integrante, Long> {
 }
