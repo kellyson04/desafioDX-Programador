3c3ff1f7ccc7c0e7dfe8bb1684272cc445fcbb1c
Kell <gilsonvigilante@outlook.com>
2026-09-09 01:35:54 -0300

    feat: cria service com lógica de buscar clube mais recorrente implementada

--- src/main/java/br/com/duxusdesafio/service/ProcessamentoService.java
+++ src/main/java/br/com/duxusdesafio/service/ProcessamentoService.java
@@ -1,4 +1,36 @@
 package br.com.duxusdesafio.service;
 
+import br.com.duxusdesafio.model.Time;
+import br.com.duxusdesafio.repository.TimeRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class ProcessamentoService {
+
+    private final TimeRepository timeRepository;
+    private final ApiService apiService;
+
+    public ProcessamentoService(
+            TimeRepository timeRepository,
+            ApiService apiService) {
+
+        this.timeRepository = timeRepository;
+        this.apiService = apiService;
+    }
+
+    public String clubeMaisRecorrente(
+            LocalDate dataInicial,
+            LocalDate dataFinal) {
+
+        List<Time> todosOsTimes = timeRepository.findAll();
+
+        return apiService.clubeMaisRecorrente(
+                dataInicial,
+                dataFinal,
+                todosOsTimes
+        );
+    }
 }
