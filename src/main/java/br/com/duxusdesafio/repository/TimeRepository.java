1ddca0f59a78eb603852d1f1ded641cc8dd5c3d4
Kell <gilsonvigilante@outlook.com>
2026-09-08 22:38:21 -0300

    feat: adiciona repositório de times pra persistencia no banco

--- src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
@@ -1,4 +1,8 @@
 package br.com.duxusdesafio.repository;
 
-public interface TimeRepository {
+import br.com.duxusdesafio.model.Time;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeRepository
+        extends JpaRepository<Time, Long> {
 }
