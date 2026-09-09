0f6908ddba4aa1e99b36a3c2097fa5b69d761cd0
Kell <gilsonvigilante@outlook.com>
2026-09-09 13:34:16 -0300

    feat: adiciona os endpoints restantes pra consumir a lógica da ApiService via web

--- src/main/java/br/com/duxusdesafio/controller/ProcessamentoController.java
+++ src/main/java/br/com/duxusdesafio/controller/ProcessamentoController.java
@@ -1,5 +1,7 @@
 package br.com.duxusdesafio.controller;
 
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.model.Time;
 import br.com.duxusdesafio.service.ProcessamentoService;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/processamentos")
@@ -17,10 +21,57 @@ public class ProcessamentoController {
 
     private final ProcessamentoService processamentoService;
 
-    public ProcessamentoController(
-            ProcessamentoService processamentoService) {
+    public ProcessamentoController(ProcessamentoService processamentoService) {
 
         this.processamentoService = processamentoService;
+    }
+
+    @GetMapping("/time-da-data")
+    public ResponseEntity<Time> timeDaData(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate data) {
+
+        return ResponseEntity.ok(processamentoService.timeDaData(data));
+    }
+
+    @GetMapping("/integrante-mais-usado")
+    public ResponseEntity<Integrante> integranteMaisUsado(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataInicial,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataFinal) {
+
+        return ResponseEntity.ok(processamentoService.integranteMaisUsado(dataInicial, dataFinal));
+    }
+
+    @GetMapping("/integrantes-do-time-mais-recorrente")
+    public ResponseEntity<List<String>> integrantesDoTimeMaisRecorrente(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataInicial,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataFinal) {
+
+        return ResponseEntity.ok(processamentoService.integrantesDoTimeMaisRecorrente(dataInicial, dataFinal));
+    }
+
+    @GetMapping("/funcao-mais-recorrente")
+    public ResponseEntity<String> funcaoMaisRecorrente(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataInicial,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataFinal) {
+
+        return ResponseEntity.ok(processamentoService.funcaoMaisRecorrente(dataInicial, dataFinal));
     }
 
     @GetMapping("/clube-mais-recorrente")
@@ -33,12 +84,32 @@ public class ProcessamentoController {
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             LocalDate dataFinal) {
 
-        String resultado =
-                processamentoService.clubeMaisRecorrente(
-                        dataInicial,
-                        dataFinal
-                );
+        return ResponseEntity.ok(processamentoService.clubeMaisRecorrente(dataInicial, dataFinal));
+    }
 
-        return ResponseEntity.ok(resultado);
+    @GetMapping("/contagem-de-clubes")
+    public ResponseEntity<Map<String, Long>> contagemDeClubesNoPeriodo(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataInicial,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataFinal) {
+
+        return ResponseEntity.ok(processamentoService.contagemDeClubesNoPeriodo(dataInicial, dataFinal));
+    }
+
+    @GetMapping("/contagem-por-funcao")
+    public ResponseEntity<Map<String, Long>> contagemPorFuncao(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataInicial,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dataFinal) {
+
+        return ResponseEntity.ok(processamentoService.contagemPorFuncao(dataInicial, dataFinal));
     }
 }
