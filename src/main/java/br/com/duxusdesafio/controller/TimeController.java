98fbdcfabca084301418728d5ec3af27e832dfe6
Kell <gilsonvigilante@outlook.com>
2026-09-08 22:37:10 -0300

    feat: cria controller com endpoint de cadastro de times

--- src/main/java/br/com/duxusdesafio/controller/TimeController.java
+++ src/main/java/br/com/duxusdesafio/controller/TimeController.java
@@ -1,4 +1,35 @@
 package br.com.duxusdesafio.controller;
 
+import br.com.duxusdesafio.dto.CriarTimeRequest;
+import br.com.duxusdesafio.service.TimeService;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.status;
+
+@RestController
+@RequestMapping("/times")
 public class TimeController {
+
+    private final TimeService timeService;
+
+    public TimeController(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> cadastrar(@RequestBody @Valid CriarTimeRequest request) {
+        timeService.cadastrar(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
 }
